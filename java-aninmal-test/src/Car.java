cedb9d72aa3b86c843c717149412cd9dfe6c0a4d
chenqiu <1551085611@qq.com>
2020-11-21 17:40:53 +0800

    test

---
+++ java-aninmal-test/src/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+    private int ab;
+
+    public void setAb(int ab){
+        this.ab = ab;
+    }
+    public int getAb(){
+        return ab;
+    }
+
+
+    //实例化
+   // Car c = new Car();
+}
---
+++ java-aninmal-test/src/Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+    public static void main(String[] args) {
+        Car name = new Car();
+        name.setAb(79);
+        int a= name.getAb();
+        System.out.println(a);
+    }
+}
